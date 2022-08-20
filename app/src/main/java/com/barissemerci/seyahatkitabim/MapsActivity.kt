2ebd3d82de2d1387283befc1ca5490439b2a0c63
barissemerci <barissemerci3@gmail.com>
2022-08-20 23:41:43 +0300

    Konum izinleri

--- app/src/main/java/com/barissemerci/seyahatkitabim/MapsActivity.kt
+++ app/src/main/java/com/barissemerci/seyahatkitabim/MapsActivity.kt
@@ -1,7 +1,18 @@
 package com.barissemerci.seyahatkitabim
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +21,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.barissemerci.seyahatkitabim.databinding.ActivityMapsBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private lateinit var permissionLauncer:ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +41,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        registerLauncher()
     }
 
     /**
@@ -40,9 +56,57 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    locationManager=this.getSystemService(LOCATION_SERVICE) as LocationManager
+        locationListener   = object :LocationListener {
+            override fun onLocationChanged(location: Location) {
+                println("location:" + location.toString())
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                super.onStatusChanged(provider, status, extras)
+            }
+        }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.root,"Permission needed for location",Snackbar.LENGTH_LONG).setAction("Give permission"){
+                    permissionLauncer.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                }.show();
+
+            }
+            else{
+                permissionLauncer.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+
+            }
+        }
+
+        else{ //izin varsa
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+
+        }
+
+
+
+    }
+    private fun registerLauncher(){
+        permissionLauncer=registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            result ->
+            if(result){
+                //permission granted
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,
+                        0f,
+                        locationListener
+                    )
+                }
+            }
+            else{
+                //permission denied
+                Toast.makeText(this@MapsActivity,"Permission needed", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
