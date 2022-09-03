d302a3da5fbcdec19cd5138109578d7f29e97b3d
barissemerci <barissemerci3@gmail.com>
2022-09-03 18:47:55 +0300

    dao ve database eklendi

---
+++ app/src/main/java/com/barissemerci/seyahatkitabim/roomdb/PlaceDatabase.kt
@@ -0,0 +1,10 @@
+package com.barissemerci.seyahatkitabim.roomdb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.barissemerci.seyahatkitabim.model.Place
+
+@Database(entities = [Place::class],version = 1)
+abstract class PlaceDatabase : RoomDatabase(){
+    abstract fun placeDao():PlaceDao
+}
